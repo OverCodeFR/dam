347ca783575b09af3ab3e1c996dfa4e17bf8a18f
christopher-gerard <christopher.gerard85@gmail.com>
2025-05-27 11:04:53 +0200

    Initial Migrations

---
+++ database/migrations/2025_05_27_075522_create_treatments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('treatments', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('name');
+            $table->float('dosage');
+            $table->date('start_at');
+            $table->date('end_at');
+            $table->foreign('patient_id')->references('id')->on('patients');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('treatments');
+    }
+};
