3b84e45c47920a796b53309d70bdb51818c3e4ce
quentin-bln <blaineau.quentin@gmail.com>
2025-05-27 11:49:34 +0200

    models

--- app/Models/TreatmentFrequency.php
+++ app/Models/TreatmentFrequency.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TreatmentFrequency extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'amount',
+    ];
+
+    protected function frequencies(): HasMany
+    {
+        return $this->hasMany(Frequency::class);
+    }
+
+    protected function treatments(): HasMany
+    {
+        return $this->hasMany(Treatment::class);
+    }
 }
--- app/Models/TreatmentType.php
+++ app/Models/TreatmentType.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TreatmentType extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'module',
+    ];
+
+    protected function treatments(): HasMany
+    {
+        return $this->hasMany(Treatment::class);
+    }
+
 }
