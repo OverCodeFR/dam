0a7403c4d651443c446031787d46926f6bee8e20
quentin-bln <blaineau.quentin@gmail.com>
2025-06-03 16:13:52 +0200

    patient form and fix treatment form

---
+++ resources/views/components/form/submit-button.blade.php
@@ -0,0 +1,7 @@
+<!-- resources/views/components/button-submit.blade.php -->
+<div class="mt-6">
+    <button type="submit"
+            class="inline-flex items-center px-3 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-white hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500">
+        {{ $label }}
+    </button>
+</div>
