1d5047deca5fe26f7b68c6d3c4e6c9d24dcc02c1
christopher-gerard <christopher.gerard85@gmail.com>
2025-06-16 16:15:58 +0200

    APIÃ

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,12 +8,13 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,45 @@
 <?php
 
+use App\Models\Patient;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserController;
 
-//Route::get('/user', function (Request $request) {
-//    return $request->user();
-//})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->get('/users', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/patients', function () {return Patient::all();});
 
 
-Route::get('/users', [UserController::class, 'index']);
+
+//Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
+////    return \App\Models\Patient::();
+//    return Auth::guard('patient');
+//});
+
+Route::middleware(['auth:sanctum'])->get('/patients/{id}/token', function ($id) {
+    $patient = Patient::find($id);
+
+    if (!$patient) {
+        return response()->json(['message' => 'Patient not found'], 404);
+    }
+
+    $patient->tokens()->where('name', 'access_token')->delete();
+    $token = $patient->createToken('access_token')->plainTextToken;
+
+    return response()->json([
+        'token' => $token,
+        'patient' => $patient,
+    ]);
+});
+
+
+//
+//Route::middleware('auth:sanctum')->get('/me', function () {
+//    return Auth::guard('patient')->user();
+//});
+
+//Route::apiResource('users', UserController::class);
+
 
